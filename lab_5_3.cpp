94c8701ae02135bcfffcd11f4a63e1f0a7d094f9
Unknown <mymintzata@hotmail.co.th>
2017-12-04 17:54:54 +0700

    ทำข้อที่ 3 เสร็จ

--- lab_5_3.cpp
+++ lab_5_3.cpp
@@ -8,10 +8,16 @@ int main() {
     cout << "Enter your score(0-100) : ";
     cin >> score;
     cout << endl;
-    if(score > 80) {
+    if(score > 79) {
         cout << "Grade 4" << endl;
-    } else if (score > 70) {
+    } else if (score > 69) {
         cout << "Grade 3" << endl;
+    } else if (score > 59) {
+        cout << "Grade 2" << endl;
+    } else if (score > 49) {
+        cout << "Grade 1" << endl;
+    } else  {
+        cout << "Grade 0" << endl;
     }
     return 0;
 }
